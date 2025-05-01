e32dc2bc90fc844aea13065ca270a7e6f9509fbd
quickarabica <cse22067@iiitkalyani.ac.in>
2025-05-01 14:37:38 +0530

    editted

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ features = soil_features + weather_features
 
 # Function to load crop image
 def load_crop_image(crop_name):
-    img_path = os.path.join("CropPredictor", "assets", f"{crop_name.lower()}.jpg")
+    img_path = os.path.join("assets", f"{crop_name.lower()}.jpg")
     if os.path.exists(img_path):
         return Image.open(img_path)
     else:
